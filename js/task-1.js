314bfed81a8d32dbfe7d7c1f4fd884b9517608f7
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-02-13 12:27:47 +0200

    performance of the task-1

--- js/task-1.js
+++ js/task-1.js
@@ -8,15 +8,17 @@
 // containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
 // Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
 
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+  for (const product of Object.values(products)) {
+    totalProducts += product;
+  }
+  return totalProducts <= containerSize;
+}
 
-// console.log(
-//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-// ); // true
+// console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
-// console.log(
-//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-// ); // false
+// console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 
 // console.log(
 //   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
